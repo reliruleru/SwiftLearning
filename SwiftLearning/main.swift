fd06b16c8a8dd6412db61d38a9f9be694ff159c5
Thermidor <reliruleru@macbook-air.local>
2016-07-28 21:19:19 +0900

    Initial Commit

---
+++ SwiftLearning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLearning
+//
+//  Created by Thermidor on 2016/07/28.
+//  Copyright © 2016年 Thermidor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
